5abde4bb41329debe10a4c1260f4b13591c0d9a4
AncaPeter <ancapeter90@gmail.com>
2020-10-31 20:29:23 +0200

    * added more contacts infos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import {FormsModule} from '@angular/forms';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { ContactSearchComponent } from './contact-search/contact-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    ContactsComponent
+    ContactsComponent,
+    ContactInfoComponent,
+    ContactSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/contact.ts
+++ src/app/contact.ts
@@ -1,5 +1,5 @@
 export interface Contact {
   id: number;
   name: string;
-  // TODO: add more infos (description, image)
+  description: string;
 }
